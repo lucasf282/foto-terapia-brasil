25cf89330a57f0715537b1c4729e33ac93806911
Lucas Farias <lucasfsiqueira282@gmail.com>
2019-11-20 20:16:24 -0300

    create sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  faBars = faBars
   title = 'foto-terapia-brasil';
+  public toggleMenuStatus: boolean = false;
+
+  toggleMenu() {
+    this.toggleMenuStatus = !this.toggleMenuStatus;
+  }
 }
